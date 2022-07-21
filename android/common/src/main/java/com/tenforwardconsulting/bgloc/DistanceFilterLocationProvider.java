73e9332a42d33fddd36fb9dedd0d3c5e5537dd58
fes-mac-note <kenshi@fes-total.com>
2022-07-21 23:49:18 +0900

    import

--- android/common/src/main/java/com/tenforwardconsulting/bgloc/DistanceFilterLocationProvider.java
+++ android/common/src/main/java/com/tenforwardconsulting/bgloc/DistanceFilterLocationProvider.java
@@ -21,6 +21,7 @@ import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.marianhello.bgloc.Config;
